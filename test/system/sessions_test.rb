2fb907f44a56e5b5a4a51c3ddc8079dc816869d7
niikz <60736158+niikz@users.noreply.github.com>
2022-11-27 23:11:38 +0900

    ログアウト処理のテストを追加

--- test/system/sessions_test.rb
+++ test/system/sessions_test.rb
@@ -12,4 +12,13 @@ class SessionsTest < ApplicationSystemTestCase
     click_on 'ログイン'
     assert_text 'ログインしました'
   end
+
+  test 'logout' do
+    twitter_login_setup!
+    visit root_url
+    click_on 'ログイン'
+    assert_text 'ログインしました'
+    click_on 'ログアウト'
+    assert_text 'ログアウトしました'
+  end
 end
