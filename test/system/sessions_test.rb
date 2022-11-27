1a7f0c68d87dfcec8c7722f7c63e84c99ac99772
niikz <60736158+niikz@users.noreply.github.com>
2022-11-27 23:11:28 +0900

    ログイン処理のテストを追加

---
+++ test/system/sessions_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class SessionsTest < ApplicationSystemTestCase
+  include OmniauthTwitterHepler
+
+  test 'login using twitter' do
+    twitter_login_setup!
+    visit root_url
+    assert_selector 'h1', text: 'WaterMark Me'
+    click_on 'ログイン'
+    assert_text 'ログインしました'
+  end
+end
