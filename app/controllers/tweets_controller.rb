67edcd1578d23957c9238ace9e35076df74577a7
niikz <60736158+niikz@users.noreply.github.com>
2022-12-06 00:47:01 +0900

    Tweet投稿機能を実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,13 @@ class TweetsController < ApplicationController
 
   def new; end
 
-  def create; end
+  def create
+    response = twitter_client.statuses_update({
+                                                status: tweet_params
+                                              })
+    JSON.pretty_generate(JSON.parse(response.body))
+    redirect_to new_tweet_path, notice: 'ツイートを投稿しました'
+  end
 
   private
 
@@ -16,5 +22,9 @@ class TweetsController < ApplicationController
                              token: current_user.token,
                              token_secret: current_user.secret
                            })
+  end
+
+  def tweet_params
+    params[:description]
   end
 end
