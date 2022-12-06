6b06a74fae3179b2049c72dc4945a9d76d5923f9
niikz <60736158+niikz@users.noreply.github.com>
2022-12-06 20:17:53 +0900

    投稿機能をFormオブジェクトに移した

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,23 +9,15 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = TweetForm.new(tweet_params)
-    response = twitter_client.statuses_update({
-                                                status: tweet_params[:description]
-                                              })
-    JSON.pretty_generate(JSON.parse(response.body))
-    redirect_to new_tweet_path, notice: 'ツイートを投稿しました'
+    user = current_user
+    if @tweet.save(user)
+      redirect_to new_tweet_path, notice: 'ツイートを投稿しました'
+    else
+      render :new
+    end
   end
 
   private
-
-  def twitter_client
-    TwitterAPI::Client.new({
-                             consumer_key: ENV['TWITTER_API_KEY'],
-                             consumer_secret: ENV['TWITTER_API_SECRET'],
-                             token: current_user.token,
-                             token_secret: current_user.secret
-                           })
-  end
 
   def tweet_params
     params.require(:tweet_form).permit(:description)
--- app/forms/tweet_form.rb
+++ app/forms/tweet_form.rb
@@ -5,9 +5,24 @@ class TweetForm
 
   attr_accessor :description
 
-  def save
+  def save(user)
     return false if invalid?
 
+    response = twitter_client(user).statuses_update({
+                                                      status: description
+                                                    })
+    JSON.pretty_generate(JSON.parse(response.body))
     true
+  end
+
+  private
+
+  def twitter_client(user)
+    TwitterAPI::Client.new({
+                             consumer_key: ENV['TWITTER_API_KEY'],
+                             consumer_secret: ENV['TWITTER_API_SECRET'],
+                             token: user.token,
+                             token_secret: user.secret
+                           })
   end
 end
