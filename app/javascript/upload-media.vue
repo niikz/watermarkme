71e03771d79b46b382a036a3b2e2bf61fbea10a3
niikz <60736158+niikz@users.noreply.github.com>
2022-12-21 23:54:16 +0900

    File APIでアップロードした画像を表示

--- app/javascript/upload-media.vue
+++ app/javascript/upload-media.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <canvas id="canvas"></canvas>
+    <label for="tweet_form_media">Media</label>
+    <input
+      id="tweet_form_media"
+      ref="preview"
+      type="file"
+      @change="uploadMedia" />
+    <div id="result"></div>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
@@ -14,23 +19,33 @@ export default {
       canvasHeight: null
     }
   },
-  mounted () {
-    // 画像の準備
-    const img = new Image()
-    img.src = 'https://placehold.jp/150x150.png'
-    this.canvasWidth = img.width
-    this.canvasHeight = img.height
-
+  mounted() {
     // Canvasの準備
     this.canvas = document.getElementById('canvas')
-    this.canvas.width = this.canvasWidth
-    this.canvas.height = this.canvasHeight
-    const ctx = this.canvas.getContext('2d')
+  },
+  methods: {
+    uploadMedia() {
+      // ファイル情報を取得
+      const fileData = this.$refs.preview.files[0]
+      if (!fileData.type.match('image.*')) {
+        alert('画像を選択してください')
+        return
+      }
 
-    // Canvas上に画像を表示
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0, this.canvasWidth, this.canvasHeight)
-      return this.canvas
+      // FileReaderオブジェクトを使ってファイル読み込み
+      const reader = new FileReader()
+      {
+        // ファイル読み込みに成功したときの処理
+        reader.onload = () => {
+          // 画像の準備
+          const outputImg = new Image()
+          outputImg.src = reader.result
+          // ブラウザ上に表示
+          document.getElementById('result').appendChild(outputImg)
+        }
+      }
+      // ファイル読み込みを実行
+      reader.readAsDataURL(fileData)
     }
   }
 }
