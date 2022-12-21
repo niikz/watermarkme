8b0bd9afbbf9ac7c0e5c2a24334cd7a53a246d6c
niikz <60736158+niikz@users.noreply.github.com>
2022-12-21 23:38:13 +0900

    Canvas上に画像を表示

--- app/javascript/upload-media.vue
+++ app/javascript/upload-media.vue
@@ -1,7 +1,39 @@
 <template>
-  <div></div>
+  <div>
+    <canvas id="canvas"></canvas>
+  </div>
 </template>
 
-<script></script>
+<script>
+
+export default {
+  data() {
+    return {
+      canvas: '',
+      canvasWidth: null,
+      canvasHeight: null
+    }
+  },
+  mounted () {
+    // 画像の準備
+    const img = new Image()
+    img.src = 'https://placehold.jp/150x150.png'
+    this.canvasWidth = img.width
+    this.canvasHeight = img.height
+
+    // Canvasの準備
+    this.canvas = document.getElementById('canvas')
+    this.canvas.width = this.canvasWidth
+    this.canvas.height = this.canvasHeight
+    const ctx = this.canvas.getContext('2d')
+
+    // Canvas上に画像を表示
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, this.canvasWidth, this.canvasHeight)
+      return this.canvas
+    }
+  }
+}
+</script>
 
 <style scoped></style>
