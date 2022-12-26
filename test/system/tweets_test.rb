8f15678f68644870778e2fbe1ad0eac128a517bd
niikz <60736158+niikz@users.noreply.github.com>
2022-12-26 23:27:45 +0900

    エラーメッセージを共通化

--- test/system/tweets_test.rb
+++ test/system/tweets_test.rb
@@ -15,4 +15,16 @@ class TweetsTest < ApplicationSystemTestCase
     visit new_tweet_url
     assert_selector 'h1', text: '画像を投稿する'
   end
+
+  test 'show description too long error message' do
+    description_text = 'a' * 141
+    fill_in 'Description', with: description_text
+    click_on '投稿する'
+    assert_text 'Description is too long (maximum is 140 characters)'
+  end
+
+  test 'show media empty error message' do
+    click_on '投稿する'
+    assert_text "Media can't be blank"
+  end
 end
