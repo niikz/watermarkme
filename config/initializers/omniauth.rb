d62da2718a772dd200a9f2add13c497fb6fd8d62
niikz <60736158+niikz@users.noreply.github.com>
2023-01-02 22:35:51 +0900

    production環境のTwitter API Key設定を追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+  if Rails.env.production?
+    provider :twitter, Rails.application.credentials.twitter[:twitter_api_key], Rails.application.credentials.twitter[:twitter_api_secret]
+  else
+    provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+  end
 end
