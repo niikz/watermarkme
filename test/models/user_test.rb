2ee419af35be4544f6df315783449978d81b7eba
niikz <60736158+niikz@users.noreply.github.com>
2023-01-20 23:42:19 +0900

    新規ユーザーの場合のテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'new user' do
+    auth_hash = OmniAuth::AuthHash.new({
+                                         'provider' => 'twitter',
+                                         'uid' => '123456',
+                                         'info' => {
+                                           'nickname' => 'OmniAuth Twitter',
+                                           'name' => 'OmniAuth Twitter Hepler',
+                                           'image' => 'http://example.com'
+                                         },
+                                         'credentials' => {
+                                           'token' => 'test_token',
+                                           'secret' => 'test_token_secret'
+                                         }
+                                       })
+    assert_difference 'User.count' do
+      User.find_or_create_from_auth_hash(auth_hash)
+    end
+  end
 end
