1a6592f3e563205a33eb9c4065cdab51178aec28
niikz <60736158+niikz@users.noreply.github.com>
2023-01-20 23:43:45 +0900

    既に作成されたユーザーの場合のテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,4 +21,25 @@ class UserTest < ActiveSupport::TestCase
       User.find_or_create_from_auth_hash(auth_hash)
     end
   end
+
+  test 'existing user' do
+    auth_hash = OmniAuth::AuthHash.new({
+                                         'provider' => 'twitter',
+                                         'uid' => '18651126',
+                                         'info' => {
+                                           'nickname' => 'alice',
+                                           'name' => 'alice world',
+                                           'image' => 'http://example.com/alice.jpg'
+                                         },
+                                         'credentials' => {
+                                           'token' => 'token',
+                                           'secret' => 'secret'
+                                         }
+                                       })
+    user = User.find_or_create_from_auth_hash(auth_hash)
+    assert_equal user.uid, users(:alice).uid
+    assert_no_difference 'User.count' do
+      User.find_or_create_from_auth_hash(auth_hash)
+    end
+  end
 end
